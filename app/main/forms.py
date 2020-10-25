258ee34b92963bae64f95f7ec9c0ee05f61a691c
joansaj <joannsaj@gmail.com>
2020-10-25 23:45:16 +0300

    added user test

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,3 +7,7 @@ class PitchForm(FlaskForm):
     title = StringField('Pitch title',validators=[Required()])
     pitch = TextAreaField('Pitch', validators=[Required()])
     submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')    
