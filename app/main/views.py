3e4e11764fd2a553738c55919ad2dc1db8acb57d
joansaj <joannsaj@gmail.com>
2020-10-27 00:13:52 +0300

    add edits

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
-from ..models import User,Pitch, Comment#, Upvote, Downvote
+from ..models import User,Pitch, Comment, Upvote, Downvote
 from flask_login import login_required, current_user
 from .. import db
 from .forms import PitchForm, UpdateProfile , CommentForm#, Upvote, Downvote
@@ -20,6 +20,13 @@ def index():
     pickup = Pitch.query.filter_by(category = 'Pickup-lines')
     title = 'Home||Pitches'
     return render_template('index.html', title = title, pitches = pitches, interview = interview, slogan = slogan, advertisements = advertisements, pickup = pickup)
+
+def votes():
+    pitches = Pitch.query.all()
+    likes = Upvote.query.all()
+    dislikes = Downvote.query.all()
+    user = current_user
+    return render_template('index.html', pitches=pitches, likes=likes, user=user, dislikes = dislikes)
 
 @main.route('/pitches', methods = ['GET','POST'])
 @login_required
@@ -87,3 +94,20 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
 
     return render_template('profile/update.html',form =form)
+
+
+@main.route('/like/<int:pitch_id>', methods=['POST', 'GET'])
+@login_required
+def upvote(pitch_id):
+    pitches = Pitch.query.get(id )
+    like = Upvote( upvote=1)
+    like.save_upvote()
+    return redirect(url_for('.upvote'))    
+
+@main.route('/dislike/<int:pitch_id>', methods=['POST', 'GET'])
+@login_required
+def downvote(pitch_id):
+    pitches = Pitch.query.get(id)
+    dislike = Downvote( downvote=1)
+    dislike.save_downvote()
+    return redirect(url_for('.downvote'))      
