25ea12cd7c97500f9906d26e0497c17dbe92a10f
joansaj <joannsaj@gmail.com>
2020-10-26 08:04:11 +0300

    added email

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,15 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+mail = Mail()
+
 def create_app(config_name):
     app = Flask(__name__)
 
@@ -19,7 +22,8 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-    
+    mail.init_app(app)
+
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
